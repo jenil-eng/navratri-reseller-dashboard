cef2917800d4754f1cf69376e40ba7c81ff9101d
Jenil Amipara <jenilamipara2005@gmail.com>
2026-08-20 17:21:24 +0530

    feat: make API base URL configurable via environment variable

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const API_BASE_URL = import.meta.env.VITE_API_URL 
+  ? `${import.meta.env.VITE_API_URL.replace(/\/$/, '')}/api` 
+  : '/api';
+
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   }
